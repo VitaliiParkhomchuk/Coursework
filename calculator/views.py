a39e5f8d357b2f6d7ec1715a4c60dd622d29824b
Vitalii <x4gm9k@gmail.com>
2024-05-06 14:07:57 +0300

    Add heating calculation

--- calculator/views.py
+++ calculator/views.py
@@ -9,6 +9,8 @@ import python_weather
 import asyncio
 import os
 
+import json
+
 def calculator(request):
     data = Calculation.objects.all()
     return render(request, 'calculator/calculator.html', {'data': data})
@@ -46,14 +48,27 @@ async def get_weather_time_temperature(city):
 
 def result(request):
     obj = Calculation.objects.order_by('-id').first()
-    res = obj.area + obj.tempNeed + obj.tempNow
     city = str(obj.city)
     weatherForecast = get_weather_time_temperature(city)
-    
+
+
+    heating = [item for sublist in weatherForecast for item in sublist]
+    tempInside = obj.tempNow
+    for item in range(1, len(heating), 2):
+        heating[item] = round((100 - heating[item] + 100 - tempInside + 100 - obj.tempNeed) * 0.08 + obj.area * 0.03, 1)
+        tempInside = heating[item]
+
+
+    heating_json = json.dumps(heating)
+    with open('calculator/static/calculator/json/heating.json', 'w') as f:
+        f.write(heating_json)
+
+
     data = {
         'city':city,
-        'res':res,
-        'weatherForecast':weatherForecast
+        'weatherForecast':weatherForecast,
+        'heating':heating,
+        'obj':obj
     }
     
     return render(request, 'calculator/result.html', {'data':data})
